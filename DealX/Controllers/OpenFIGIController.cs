31988d5741b01289be8f67f5ecb3fdae29531d97
iCreature <mxolisi102@gamil.com>
2022-03-07 21:41:31 +0200

    Add project files.

---
+++ DealX/Controllers/OpenFIGIController.cs
@@ -0,0 +1,47 @@
+﻿using DealX.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace DealX.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OpenFIGIController : ControllerBase
+    {
+
+        static HttpClient theClient = new HttpClient();
+        string theURL="https://api.openfigi.com/v3/filter/";
+        // store the request
+
+        // get information from OpenFIGI
+        [HttpGet("getInformation")]
+        public async Task<IActionResult> get()
+        {
+
+            var response = await theClient.PostAsJsonAsync(theURL, "{'exchCode' : 'SA'}");
+            response.EnsureSuccessStatusCode();
+            return Ok(response.Headers.Location);
+        }
+
+        // post -> to search information from OpenFIGI
+        [HttpPost("searchInformation")] 
+        public IActionResult search([FromBody] UserRequest pRequest)
+        {
+            return Ok();
+        }
+
+        /*
+         *https://api.openfigi.com/v3/filter/
+         *{
+            "exchCode": "SA"
+            }
+         */
+    }
+}
---
+++ DealX/Models/UserRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DealX.Models
+{
+    public class UserRequest
+    {
+        public string theName { get; set; }
+        public string theFIGI { get; set; }
+        public string theTicker { get; set; }
+        public string theExchCode { get; set; }
+        public string theSecuritytype { get; set; }
+        public string theMarketSector { get; set; }
+
+    }
+}
